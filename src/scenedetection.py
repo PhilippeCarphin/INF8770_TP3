866f0526f9049c2d080539a22690b48ec61fab2f
Adam Martin-Côté <adammartincote@gmail.com>
2019-04-01 20:49:54 -0600

    adds a naive scene cut detection

--- src/scenedetection.py
+++ src/scenedetection.py
@@ -1,5 +1,14 @@
+""" Scene detection on video sequence using different algorithms
+
+references:
+https://bcastell.com/posts/scene-detection-tutorial-part-1/
+
+"""
 import sys
 import cv2
+
+from matplotlib import pyplot as plt
+
 
 def main():
     if len(sys.argv) < 2:
@@ -15,10 +24,34 @@ def main():
     else:
         print("Parsing video %s..." % sys.argv[1])
 
-    # Do stuff with cap here.
+    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    print("Video Resolution: %d x %d" % (width, height))
+
+    means = []
+    cuts = []
+    while True:
+        (rv, im) = cap.read()   # im is a valid image if and only if rv is true
+        if not rv:
+            break
+        frame_mean = im.mean()
+        threshold = 20
+        if means and abs(frame_mean - means[-1]) > threshold:
+            frame_no = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+            cuts.append(frame_no)
+
+        means.append(frame_mean)
+
+    frame_count = cap.get(cv2.CAP_PROP_POS_FRAMES)  # current capture position
+    print("Read %d frames from video." % frame_count)
+    print("cuts detected on the following frames: ")
+    print(cuts)
+
+    plt.plot(means)
+    plt.title("Mean variation through the video")
+    plt.show()
 
     cap.release()
-
 
 if __name__ == "__main__":
     main()
